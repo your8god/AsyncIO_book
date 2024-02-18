8d7eda91352aa7dbcc1798d9c6f2658d43f19043
Богдан <bogdan.terebin@ya.ru>
2024-02-18 22:41:32 +0400

    echo server asyncio

--- my_server.py
+++ my_server.py
@@ -1,6 +1,23 @@
 import socket
 import asyncio
+import signal
 
+
+class ExitException(SystemExit):
+    pass
+
+def exit_raise():
+    raise ExitException
+
+tasks = []
+
+async def close_echo(tasks):
+    waiters = [asyncio.wait_for(task, 2) for task in tasks]
+    for task in waiters:
+        try:
+            await task
+        except TimeoutError:
+            pass
 
 async def echo(connection, loop, address):
     while data := await loop.sock_recv(connection, 1024):
@@ -13,7 +30,8 @@ async def listening(server_socket, loop):
         connection, client_address = await loop.sock_accept(server_socket)
         connection.setblocking(False)
         print(f'Получен запрос на подключение {client_address}')
-        asyncio.create_task(echo(connection, loop, client_address))
+        task = asyncio.create_task(echo(connection, loop, client_address))
+        tasks.append(task)
 
 
 async def main():
@@ -25,7 +43,16 @@ async def main():
     server_socket.bind(server_address)
     server_socket.listen()
 
+    for item in ('SIGINT', 'SIGTERM'):
+        main_loop.add_signal_handler(getattr(signal, item), exit_raise)
+
     await listening(server_socket, asyncio.get_event_loop())
 
 
-asyncio.run(main())
+main_loop = asyncio.new_event_loop()
+try:
+    main_loop.run_until_complete(main())
+except ExitException:
+    main_loop.run_until_complete(close_echo(tasks))
+finally:
+    main_loop.close()
